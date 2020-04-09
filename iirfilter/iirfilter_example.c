fa206c94fbc1793a2da84d9b7727db02864f8da8
Cuda-Chen <clh960524@gmail.com>
2020-04-09 15:37:03 +0800

    iirfilter: change to band pass fitler

--- iirfilter/iirfilter_example.c
+++ iirfilter/iirfilter_example.c
@@ -10,11 +10,12 @@ int main()
 {
     // options
     liquid_iirdes_filtertype ftype = LIQUID_IIRDES_BUTTER;
-    liquid_iirdes_bandtype btype = LIQUID_IIRDES_LOWPASS;
+    //liquid_iirdes_bandtype btype = LIQUID_IIRDES_LOWPASS;
+    liquid_iirdes_bandtype btype = LIQUID_IIRDES_BANDPASS;
     liquid_iirdes_format     format = LIQUID_IIRDES_SOS;
     unsigned int order = 4; // filter order
     float fc = 0.1f; // cutoff frequency
-    float f0 = 0.0f; // center frequency
+    float f0 = 0.3f; // center frequency
     float Ap = 1.0f; // pass-band ripple
     float As = 40.0f; // stop-band attenuation
     unsigned int n = 128; // number of samples
